05bf39038098dd9d395cdb102740dca602e3af78
Andrew Johnson <anj@aps.anl.gov>
2016-07-05 15:07:56 -0500

    Modify testBoost so it only needs shared_ptr

--- testApp/boost/testBoost.cpp
+++ testApp/boost/testBoost.cpp
@@ -1,20 +1,18 @@
-#include <boost/atomic.hpp>
-#include <boost/tr1/memory.hpp>
-#include <boost/tr1/unordered_map.hpp>
 #include <string>
-
 #include <iostream>
 
-typedef std::tr1::unordered_map<int, std::string> test_map;
+#ifdef vxWorks
+#  include <boost/tr1/memory.hpp>
+#else
+#  include <tr1/memory>
+#endif
 
-int main()
+#include <testMain.h>
+
+MAIN(testShared)
 {
-    boost::atomic<int> ai;
-    ai = 3;
-    ai = 4;
-
-    std::tr1::shared_ptr<test_map> tm(new test_map);
-    (*tm)[1208] = "test string";
+    std::tr1::shared_ptr<double> spd(new double);
+    *spd = 1.23456789;
 
     std::cout << "Apparently it works..." << std::endl;
 
